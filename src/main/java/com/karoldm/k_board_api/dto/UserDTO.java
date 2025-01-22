91a07bf4106556d4f6f85337dfc71392fedcb2b0
karol marques <karoldm1579@gmail.com>
2025-01-22 16:19:23 -0300

    feat: add fields handler to userDTO

--- src/main/java/com/karoldm/k_board_api/dto/UserDTO.java
+++ src/main/java/com/karoldm/k_board_api/dto/UserDTO.java
@@ -1,11 +1,20 @@
 package com.karoldm.k_board_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record UserDTO(
+        @NotNull(message = "Name cannot be null")
+        @NotEmpty(message = "Name cannot be empty")
         String name,
+        @NotNull(message = "Email cannot be null")
+        @NotEmpty(message = "Email cannot be empty")
+        @Email(message = "Invalid email format")
         String email,
-        LocalDate createdAt,
-        String photoUrl
+        String photoUrl,
+        LocalDate createdAt
     ) {
 }
