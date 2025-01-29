8eb25fada15c47347be25945b1790a49d743c175
karol marques <karoldm1579@gmail.com>
2025-01-29 15:19:13 -0300

    feat: add member to a task

---
+++ src/main/java/com/karoldm/k_board_api/dto/payload/AddResponsiblePayloadDTO.java
@@ -0,0 +1,12 @@
+package com.karoldm.k_board_api.dto.payload;
+
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.Set;
+import java.util.UUID;
+
+public record AddResponsiblePayloadDTO(
+        @NotEmpty(message = "membersId cannot be empty")
+        Set<UUID> membersId
+) {
+}
