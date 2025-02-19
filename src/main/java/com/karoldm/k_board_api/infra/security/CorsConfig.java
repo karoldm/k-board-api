5e7a16dbc2c5c2be39a76016a2759a0259c28f80
karol marques <karoldm1579@gmail.com>
2025-02-19 16:18:22 -0300

    fix: cors

--- src/main/java/com/karoldm/k_board_api/infra/security/CorsConfig.java
+++ src/main/java/com/karoldm/k_board_api/infra/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow frontend origin
+        configuration.setAllowedOrigins(List.of("https://k-board.onrender.com")); // Allow frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed HTTP methods
         configuration.setAllowedHeaders(List.of("*")); // Allow all headers
         configuration.setAllowCredentials(true); // Allow credentials (cookies, authorization headers)
